509302b688b82777d37748dfe6d98a80fcc02b43
João V. Santos <joaovaledosantos@gmail.com>
2024-02-12 06:14:22 -0300

    [Aula-116] Criando funcionalidades e melhorias

--- JS/index.js
+++ JS/index.js
@@ -7,23 +7,63 @@ const inputValue = document.getElementById('kick');
 const result = document.getElementById('result');
 const btnRestart = document.getElementById('btn-restart');
 
-const numberDraw = Math.round(Math.random() * 10)
+const GuessNumber = {
+    max: 10,
+    attemptsNumber: 0,
+    numberDraw: function () {
+        return Math.round(Math.random() * this.max);
+    },
+    showButtonRestart: function () {
+        btnRestart.style.display = 'flex';
+    },
+    clearInput: function () {
+        inputValue.value = '';
+    },
+    updateAttempt: function (attempt, value) {
+        attempt.innerHTML = 'Tentativa: ' + value;
+    },
+    correctAnswear: function () {
+        this.showButtonRestart();
+        statusTitle.innerHTML = 'Parabéns, você acertou! 😎';
+        statusTitle.classList.remove('incorrect-answear');
+        statusTitle.classList.add('status-correct');
+
+        result.classList.remove('result-box-deFault');
+        result.classList.add('result-correct-answear');
+
+        this.clearInput();
+    },
+
+    incorrectAnswear: function (message) {
+        statusTitle.innerHTML = message;
+        statusTitle.classList.add('incorrect-answear');
+
+        this.clearInput();
+    },
+
+};
+
+const numberDraw = GuessNumber.numberDraw();
 
 function handleSubmit(e) {
     e.preventDefault();
 
     const kick = inputValue.value;
 
-    if (!kick){
+    if (!kick) {
         alert('Digite algum valor!');
         return;
     }
+    ;
 
-    if (numberDraw == kick){
-        alert('ACERTOU!')
-    }else if (numberDraw > kick){
-        alert('O número é maior!')
-    }else if (numberDraw < kick){
-        alert('O número é menor!')
+    GuessNumber.updateAttempt(attempt, ++GuessNumber.attemptsNumber);
+
+    if (numberDraw == kick) {
+        GuessNumber.correctAnswear();
+    } else if (numberDraw > kick) {
+        GuessNumber.incorrectAnswear('O número é maior!');
+    } else if (numberDraw < kick) {
+        GuessNumber.incorrectAnswear('O número é menor!');
     }
 };
+
